9143cedea127673ae2a93f21b43f7eb60918a8c3
supercopoadm <injexprodadm@gmail.com>
2024-10-31 08:10:47 -0300

    add horas producaopcp

--- superprod-api/settings.py
+++ superprod-api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$&djva4%v94@(1u#yrv!lq#$g^oto*dya%xe_&u)ez@@kc2luw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "http://localhost:4200",
@@ -112,27 +112,27 @@ WSGI_APPLICATION = 'superprod-api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'superprod',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'superprod',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': '192.168.90.80',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
 #         'PORT': '3306',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'superprod',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '192.168.90.80',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
@@ -159,7 +159,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
